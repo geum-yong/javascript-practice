5f3dad5b3b7b41ee9df2abda911da47123d4b3ab
Ben <geumyong@carrotins.com>
2023-11-05 00:26:39 +0900

    virtualized list css 추가

--- src/ts/virtualized-list.ts
+++ src/ts/virtualized-list.ts
@@ -17,9 +17,6 @@ const visibleItems = Math.ceil(Number(listContainerHeight) / Number(itemSize));
 const setInitContainerSize = () => {
   listContainer.style.width = `${listContainerWidth}px`;
   listContainer.style.height = `${listContainerHeight}px`;
-  listContainer.style.overflow = "auto";
-  listContainer.style.backgroundColor = "#fff";
-  listContainer.style.position = "relative";
 
   listBox = document.createElement("div");
   listBox.style.height = `${Number(itemCount) * Number(itemSize)}px`;
@@ -40,32 +37,13 @@ const setListData = () => {
   listBox.innerHTML = "";
   for (let i = start; i < end; i++) {
     const listItem = document.createElement("div");
-    listItem.style.width = "100%";
+    listItem.classList.add("list-item");
     listItem.style.height = `${itemSize}px`;
-    listItem.style.padding = "8px 16px";
-    listItem.style.display = "flex";
-    listItem.style.alignItems = "center";
-    listItem.style.position = "absolute";
     listItem.style.top = `${i * Number(itemSize)}px`;
-    listItem.style.left = "0";
-
-    listItem.addEventListener("mouseenter", onMouseEnterItem);
-    listItem.addEventListener("mouseleave", onMouseOutItem);
 
     listItem.innerText = data[i];
     listBox.appendChild(listItem);
   }
-};
-
-const onMouseEnterItem = (e: MouseEvent) => {
-  const item = e.target as HTMLDivElement;
-  item.style.backgroundColor = "rgba(0, 0, 0, 0.04)";
-  item.style.cursor = "pointer";
-};
-
-const onMouseOutItem = (e: MouseEvent) => {
-  const item = e.target as HTMLDivElement;
-  item.style.backgroundColor = "#fff";
 };
 
 window.onload = () => {
